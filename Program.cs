750327414c5bd20d08ec8c9a4c8c28729c807147
ShivamSaini490 <126521640+ShivamSaini490@users.noreply.github.com>
2023-12-04 15:01:18 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+class Program
+{
+    static void Main()
+    {
+        try
+        {
+            int[] numbers = { 1, 2, 3 };
+            int value = numbers[10];
+            Console.WriteLine("This line will not be executed if an exception occurs.");
+        }
+        catch (IndexOutOfRangeException ex)
+        {
+            Console.WriteLine("Error: " + ex.Message);
+        }
+        catch (FormatException ex)
+        {
+            Console.WriteLine("Error: " + ex.Message);
+        }
+        catch (InvalidOperationException ex)
+        {
+            Console.WriteLine("Error: " + ex.Message);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("An unexpected error occurred: " + ex.Message);
+        }
+        finally
+        {
+            Console.WriteLine("This block will always be executed.");
+        }
+
+        Console.WriteLine("Program continues...");
+    }
+}
